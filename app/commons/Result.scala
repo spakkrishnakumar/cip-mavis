691db57b9cf7f984023dc6969e244ac253450b12
Akhil Krishnakumar <akhil.krishnakumar@springernature.com>
2020-06-23 10:56:46 +0530

    refactor Result trait to include common operations

--- app/commons/Result.scala
+++ app/commons/Result.scala
@@ -1,6 +1,20 @@
 package commons
 
-sealed trait Result[+Err, +A] extends Product
+sealed trait Result[+Err, +A] extends Product {
+
+  def map[B](f: A => B): Result[Err, B] =
+    this match {
+      case Success(value) => Success(f(value))
+      case Failure(err)   => Failure(err)
+    }
+
+  def fold[T](onSuccess: A => T, onFailure: Err => T): T =
+    this match {
+      case Success(value) => onSuccess(value)
+      case Failure(err)   => onFailure(err)
+    }
+
+}
 case class Success[A](value: A) extends Result[Nothing, A]
 case class Failure[Err](err: Err) extends Result[Err, Nothing]
 
@@ -8,24 +22,6 @@ object Result {
 
   implicit class OnReceiver[A](value: A) {
     def asSuccess: Success[A] = Success(value)
-  }
-
-  implicit class ResultOps[Err, A](result: Result[Err, A]) {
-
-    def printHello(): Unit = println("Hello")
-
-    def map[B](f: A => B): Result[Err, B] =
-      result match {
-        case Success(value) => Success(f(value))
-        case Failure(err)   => Failure(err)
-      }
-
-    def fold[T](onSuccess: A => T, onFailure: Err => T): T =
-      result match {
-        case Success(value) => onSuccess(value)
-        case Failure(err)   => onFailure(err)
-      }
-
   }
 
 }
